515e1c053f9d8b6ca6ef09940981881c97381c61
fiantsev <sergeyfiantsev@gmail.com>
2018-04-18 19:57:06 +0300

    some changes

---
+++ AtomCollection/set.js
@@ -0,0 +1,7 @@
+/**
+ * Сет это часть enum'a
+ * например var colorSubSet = ["red", "green"]
+ * Set должен ссылаться на свой основной enumeration: set(enum)
+ * это массив, который содержит значения только из enum'a
+ * @module
+ */
--- app.js
+++ app.js
@@ -6,3 +6,25 @@ var ExhibitInterface = {
     //Actions
     
 };
+
+
+
+
+const CountryEnum = EnumAtom({Russia:1,USA:2,Japan:3,Brazil:4,German:5});
+
+const TupleEntityScheme = {
+    guid: Guid(),
+    name: ListAtom(StringAtom(0, 100)),
+        name: ListAtom(StringAtom({minLength:0,maxLength:100})),
+    title: ListAtom(StringAtom(0, 100)),
+    subtitle: ListAtom(StringAtom(0, 100)),
+    location: ListAtom(StringAtom(0, 100)),
+    description: ListAtom(StringAtom(0, 100)),
+    history: ListAtom(StringAtom(0, 100)),
+    date: TimeAtom(),
+    coverImage: UrlAtom(),
+    fields: MapAtom(),
+    catsub: EnumAtom(),
+    countries: SetAtom(CountryEnum),
+    hashtags: ListAtom(HashTagAtom())
+};
